f309a7642b29351d4c62fce889fa01c8fa847c3b
Ezee1015 <82286822+Ezee1015@users.noreply.github.com>
2022-01-01 18:15:27 -0300

    Update PlugVim.ps1

--- PlugVim.ps1
+++ PlugVim.ps1
@@ -1,2 +1,5 @@
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
+    ni $HOME/vimfiles/autoload/plug.vim -Force
+
+# curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+#   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
