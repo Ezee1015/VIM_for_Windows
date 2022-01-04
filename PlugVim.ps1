fd777bb9b16e09cc9b1091d43302c9bab049f31a
Ezee1015 <routerciscoanuitti@gmail.com>
2022-01-04 10:19:36 -0300

    Arregla errores

--- PlugVim.ps1
+++ PlugVim.ps1
@@ -1,5 +1,2 @@
 iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
     ni $HOME/vimfiles/autoload/plug.vim -Force
-
-# curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-#   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
