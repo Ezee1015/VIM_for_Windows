e8f21c324ef50939e01f75b27980b28927bc4149
Ezee1015 <82286822+Ezee1015@users.noreply.github.com>
2023-09-26 19:06:25 -0300

    Elimina el ejecutado automático y elimina a Packer

--- INSTALL - Parte 1.bat
+++ INSTALL - Parte 1.bat
@@ -4,8 +4,6 @@ Git-2.34.1-64-bit.exe
 
 xcopy /i /E "init" "C:%homepath%\AppData\Local\nvim"
 
-powershell " powershell $(cat packer.ps1) "
-
 SETLOCAL ENABLEDELAYEDEXPANSION
 SET LinkName=Neovim
 SET Esc_LinkDest=%%HOMEDRIVE%%%%HOMEPATH%%\Desktop\!LinkName!.lnk
@@ -13,14 +11,12 @@ SET Esc_LinkTarget=%CD%\nvim-win64\bin\nvim-qt.exe
 SET cSctVBS=CreateShortcut.vbs
 SET LOG=".\%~N0_runtime.log"
 ((
-  echo Set oWS = WScript.CreateObject^("WScript.Shell"^) 
+  echo Set oWS = WScript.CreateObject^("WScript.Shell"^)
   echo sLinkFile = oWS.ExpandEnvironmentStrings^("!Esc_LinkDest!"^)
-  echo Set oLink = oWS.CreateShortcut^(sLinkFile^) 
+  echo Set oLink = oWS.CreateShortcut^(sLinkFile^)
   echo oLink.TargetPath = oWS.ExpandEnvironmentStrings^("!Esc_LinkTarget!"^)
   echo oLink.Save
 )1>!cSctVBS!
 cscript //nologo .\!cSctVBS!
 DEL !cSctVBS! /f /q
 )1>>!LOG! 2>>&1
-
-nvim-win64\bin\nvim-qt.exe
